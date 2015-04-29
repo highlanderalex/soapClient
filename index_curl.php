e71170088de08e4da55d683df8ba4c250584bd91
highlanderalex <highlanderalex@rambler.ru>
2015-04-29 06:45:25 +0300

    curl

--- index_curl.php
+++ index_curl.php
@@ -1,7 +1,26 @@
 <?php
 	require_once 'libs/func.php';
-	
-	
+
+    $ch = curl_init('http://footballpool.dataaccess.eu/data/info.wso/Teams/');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $str = simplexml_load_string(curl_exec($ch));
+    curl_close($ch);
+    $data = '';
+    foreach($str->tTeamInfo as $item)
+    {
+        $data .= '<tr><td>' . $item->sName . '</td>';
+        $data .= '<td><a href="' . $item->sWikipediaURL . '">InfoWikipedia</a></td>';
+        $data .= '<td><img src="' . $item->sCountryFlagLarge . '"></td></tr>';
+    }    
+    
+    if (isset($_POST['send']))
+	{
+        $curr = curl_init('http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency=' .
+            $_POST['from'] . '&ToCurrency=' . $_POST['to']);
+         curl_setopt($curr, CURLOPT_RETURNTRANSFER, true);
+        $rez = curl_exec($curr);
+ 		$val = $_POST['from'] . '/' . $_POST['to'] . ' = ' . $rez;
+	}
 	
 	require_once 'templates/index_curl.php';
 ?>
--- templates/index_curl.php
+++ templates/index_curl.php
@@ -4,19 +4,13 @@
 <title>Football</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<script>
-	function _open( url, width, height ) {
-        window.open( url, '', 'width=' + width + ',height=' + height + ',left=' + ((window.innerWidth - width)/2) + ',top=' + 
-					((window.innerHeight - height)/2) + ',location=no,resizable=no,fullscreen=no' );
-    };
-</script>
 </head>
 <body>
 	<div style="float:right; margin-right:20px;color:red;width:130px;">
 		<?=$val;?>
 	</div>
 	<div style="float:right; margin-right:10px;">
-	<form action="index.php" method="post">
+	<form action="index_curl.php" method="post">
 		<select name="from">
 			<option value="USD" selected>USD</option>
 			<option value="UAH">UAH</option>
@@ -31,8 +25,8 @@
 		<input type="submit" name="send" value="go">
 	</form>
 	</div>
-	<h1 align="center">FIFA World Cup 2014</h1>
-	<table class="table table-striped">
+	<h1 align="center">FIFA Team</h1>
+     <table class="table table-striped">
 		<?=$data;?>
 	</table>
 </body>
